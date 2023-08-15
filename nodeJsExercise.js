eadb0384d912d17faf7ab2ab42a9aba6fbcdfb3f
unknown <mushroomlazy259@gmail.com>
2023-08-15 08:28:54 +0700

    fix, and remove todo

--- nodeJsExercise.js
+++ nodeJsExercise.js
@@ -1,8 +1,12 @@
-const apiUrl = [
-  "https://jsonplaceholder.typicode.com/users",
-  "https://jsonplaceholder.typicode.com/posts",
-  "https://jsonplaceholder.typicode.com/comments",
-];
+const rootApiUrl = "https://jsonplaceholder.typicode.com";
+const postsApiUrl = rootApiUrl + "/posts";
+const commentsApiUrl = rootApiUrl + "/comments";
+const usersApiUrl = rootApiUrl + "/users";
+// const apiUrl = [
+//   "https://jsonplaceholder.typicode.com/users",
+//   "https://jsonplaceholder.typicode.com/posts",
+//   "https://jsonplaceholder.typicode.com/comments",
+// ];
 
 const getData = async (apiUrl) => {
   try {
@@ -15,20 +19,13 @@ const getData = async (apiUrl) => {
 };
 const getPost = async (postId) => {
   try {
-    //todo : sao mình không viết hàm getData ở trên để có thể sử dụng ở chỗ này luôn em nhỉ ?
-    const getpostById = fetch(
-      `https://jsonplaceholder.typicode.com/posts/${postId}`
-    );
-    const getCommentsOfPostId = fetch(
-      `https://jsonplaceholder.typicode.com/posts/${postId}/comments`
-    );
+    const getpostById = getData(`${postsApiUrl}/${postId}`);
+    const getCommentsOfPostId = getData(`${postsApiUrl}/${postId}/comments`);
     const [post, comments] = await Promise.all([
       getpostById,
       getCommentsOfPostId,
-    ]).then((data) => Promise.all(data.map((r) => r.json())));
+    ]);
 
-    //todo: chỗ này mình có thể xử dụng phần operator có viết trong docs training để nó ngắn gọn và tường minh hơn nhé
-    // fix:
     const { userId, id, title, body } = post;
     return {
       userId,
@@ -49,13 +46,13 @@ const getPost = async (postId) => {
 (async () => {
   // get all users, posts and comments
   const [users, posts, comments] = await Promise.all([
-    getData(apiUrl[0]),
-    getData(apiUrl[1]),
-    getData(apiUrl[2]),
+    getData(rootApiUrl + "/users"),
+    getData(rootApiUrl + "/posts"),
+    getData(rootApiUrl + "/comments"),
   ]);
 
   //  3
-  //todo : có thể viết thế này cho ngắn và dê hiểu hơn nhé . Tham khảo nhé .
+
   const result = users.map((item, index) => {
     const { id, name, username, email } = item;
     return {
@@ -74,8 +71,7 @@ const getPost = async (postId) => {
   // console.log(usersWithMoreThan3Comments);
 
   //5. Reformat the data with the count of comments and posts
-  //todo: thay vì làm thế này thử làm theo spread/rest operator xem oke hơn không nhé ?
-  // fixed
+
   const reformatData = result.map((item, index) => {
     const { comments, posts, ...another } = item;
     return {
@@ -84,8 +80,8 @@ const getPost = async (postId) => {
       postsCount: posts.length,
     };
   });
-  console.log(reformatData);
-  //todo bài này có cách này viết ngắn hơn và hạn chế dùng forEach đc không ?
+  // console.log(reformatData);
+
   //6. Who is the user with the most comments/posts
   const userWithMostComments = reformatData.reduce((prev, curr) =>
     prev.commentsCount > curr.commentsCount ? prev : curr
@@ -101,5 +97,5 @@ const getPost = async (postId) => {
   );
   // console.log(sortedArray);
   //   8. Get the post with ID of 1 via API request, at the same time get comments for post ID of 1 via another API request. Merge the post data with format:
-  // console.log(await getPost(2));
+  console.log(await getPost(2));
 })();
